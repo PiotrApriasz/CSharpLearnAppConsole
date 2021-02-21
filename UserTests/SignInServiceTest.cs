ea6c6065988bdc32817cd98ca164e3d6768eef69
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-02-21 18:42:45 +0100

    Refactor FindUser test

--- UserTests/SignInServiceTest.cs
+++ UserTests/SignInServiceTest.cs
@@ -6,10 +6,18 @@ namespace UserTests
     public class SignInServiceTest
     {
         [Test]
-        public void FindUser_Test()
+        public void FindUserCorrect_Test()
         {
             var actual = SignInService.FindUser("piotrek", "123");
-            Assert.IsTrue(actual);
+            
+            Assert.Multiple((() =>
+            {
+                Assert.AreEqual("piotrek", actual.Username); 
+                Assert.AreEqual("123", actual.Password);
+                Assert.AreEqual("Piotr", actual.Name);
+                Assert.AreEqual("Apriasz", actual.LastName);
+                Assert.AreEqual("demik534@gmail.com", actual.Email);
+            }));
         }
     }
 }
