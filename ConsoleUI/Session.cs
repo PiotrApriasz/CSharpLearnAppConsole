bc250406b5c1d86d04ac1b7546c2c59f3679aced
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-03-01 22:01:31 +0100

    Create constructor and user property in Session class

---
+++ ConsoleUI/Session.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ApplicationCore;
+
+namespace ConsoleUI
+{
+    public class Session
+    {
+        #region Constructor
+
+        public Session(User user)
+        {
+            this.User = user;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public User User { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        
+
+        #endregion
+    }
+}
