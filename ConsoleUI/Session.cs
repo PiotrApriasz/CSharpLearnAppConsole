ff4f98d0e2f5f8c684d104bb5e65504cbd4d3a93
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-03-01 23:01:45 +0100

    Create pattern for main menu choices

--- ConsoleUI/Session.cs
+++ ConsoleUI/Session.cs
@@ -28,12 +28,36 @@ namespace ConsoleUI
         /// </summary>
         public void SelectScreen()
         {
-            Console.Clear();
-            ElementsUI.ScreenTop();
+            var work = true;
             
-            AnsiConsole.Markup($"Hello [green]{User.Name}![/]\n");
+            while (work)
+            {
+                Console.Clear();
+                ElementsUI.ScreenTop();
+            
+                AnsiConsole.Markup($"Hello [green]{User.Name}![/]\n");
 
-            var choice = FunctionsUI.MainScreen();
+                var choice = FunctionsUI.MainScreen();
+
+                switch (choice)
+                {
+                    case "Learn Path":
+                        Console.WriteLine("Here will be page with learn paths");
+                        Console.ReadKey();
+                        break;
+                    case "Notes":
+                        Console.WriteLine("Here will be page with learn paths");
+                        Console.ReadKey();
+                        break;
+                    case "TO DO":
+                        Console.WriteLine("Here will be page to do things");
+                        Console.ReadKey();
+                        break;
+                    case "Log out":
+                        work = false;
+                        break;
+                }
+            }
         }
 
         #endregion
